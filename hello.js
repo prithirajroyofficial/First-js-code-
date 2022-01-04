20d09e67996545543bb1cd75c5640762d481825f
prithirajroyofficial <prithirajkumar8519@gmail.com>
2022-01-04 22:40:37 +0600

    first commit

---
+++ hello.js
@@ -0,0 +1,24 @@
+var mark =12;
+if(mark<32){
+    console.log("tui fail korcis ");
+}
+else if(mark<40)
+{
+console.log(
+    " tui C paicis "
+)
+}
+else if (mark<50)
+{
+    console.log("Tui B- paicis  ")
+}
+else if (mark<60){
+    console.log( " tui B paicis ")
+}
+else if (mark<79){
+    console.log( " tui A paicis ")
+}
+else if (mark<100){
+    console.log( " tui A+ paicis ")
+}
+
